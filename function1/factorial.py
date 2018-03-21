188bb76ab2f428cc40bbbdc3a32b03df71a6fd43
Mikku jain <mikkujain22@gmail.com>
2018-03-22 00:00:15 +0530

    This is our main program

--- function1/factorial.py
+++ function1/factorial.py
@@ -1,18 +1,19 @@
-n=int(input("Enter a number"))
-#fact1=0
-a=n
-def fact(a):
-    if a<0:
+
+#a=n
+def fact(n):
+    if n<0:
         print("factorial does not exist for -ve numbers")
         print("plese give the another value")
 
-    elif a==0:
+    elif n==0:
         print("the factorial of 0 is always 1")
 
     else:
         for i in range(1,n):
-            a=a*i
+            n=n*i
+        return n
 
-fact(n)
+a=int(input("Enter a number"))
 if a>0:
-    print("factorial no ",n,"is:",a)
+    #print("factorial no ",a,"is:",n)
+    print("factorial no ", a, "is", fact(a))
